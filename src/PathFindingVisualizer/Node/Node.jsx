661a7407cdd8ce2c1489c4a655182acd287312d5
la3jiz <lazizamin8@gmail.com>
2025-07-25 21:53:24 +0200

    adding dijkastra algorithm with visual animation

--- src/PathFindingVisualizer/Node/Node.jsx
+++ src/PathFindingVisualizer/Node/Node.jsx
@@ -1,9 +1,33 @@
-import React from "react";
-import "./Node.css";
+import React from 'react';
+import './Node.css';
 
-const Node = ({ keyIndex, isStart, isFinish }) => {
-  const extraClassName = isFinish ? "node-finish" : isStart ? "node-start" : "";
-  return <div className={`node ${extraClassName}`} key={keyIndex}></div>;
+const Node = ({
+  col,
+  isFinish,
+  isStart,
+  isWall,
+  onMouseDown,
+  onMouseEnter,
+  onMouseUp,
+  row,
+}) => {
+  const extraClassName = isFinish
+    ? 'node-finish'
+    : isStart
+    ? 'node-start'
+    : isWall
+    ? 'node-wall'
+    : '';
+
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseEnter={() => onMouseEnter(row, col)}
+      onMouseUp={() => onMouseUp()}
+    ></div>
+  );
 };
 
 export default Node;
